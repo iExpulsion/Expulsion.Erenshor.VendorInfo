8d68315972c73a94e09abc2823acee31e3b8cb73
Andrew Harris <31739342+iExpulsion@users.noreply.github.com>
2025-04-17 15:26:05 -0400

    Initial Commit

---
+++ Patches/ItemIconPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Expulsion.Erenshor.VendorInfo.Patches
+{
+    [HarmonyPatch(typeof(ItemIcon), nameof(ItemIcon.OnPointerEnter))]
+    public class ItemIconPatch
+    {
+        [HarmonyPostfix]
+        public static void OnPointerEnter(ItemIcon __instance)
+        {
+            GameData.Misc.IDTitle.text = __instance.VendorSlot
+                ? $"{__instance.MyItem.ItemName}\n{__instance.MyItem.ItemValue} Gold"
+                : $"{__instance.MyItem.ItemName}\n{Mathf.RoundToInt(__instance.MyItem.ItemValue * 0.65f)} Gold";
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace Expulsion.Erenshor.VendorInfo
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string PluginGuid = "Expulsion.Erenshor.VendorInfo";
+        private const string PluginName = "VendorInfo";
+        private const string PluginVersion = "1.0.0";
+
+        private void Awake()
+        {
+            var harmonyInstance = new Harmony(PluginGuid);
+            harmonyInstance.PatchAll();
+
+            Logger.LogInfo($"Plugin {PluginName} is loaded!");
+        }
+    }
+}
